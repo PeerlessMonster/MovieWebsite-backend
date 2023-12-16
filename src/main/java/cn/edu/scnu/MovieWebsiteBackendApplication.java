460840921a20583cf02cc70b7c9443fed0b7d9cc
PeerlessMonster <franklin1919@outlook.com>
2023-12-16 23:42:29 +0800

    新建项目

---
+++ src/main/java/cn/edu/scnu/MovieWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.scnu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieWebsiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieWebsiteBackendApplication.class, args);
+	}
+
+}
